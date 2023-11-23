795153ff3b20d77ae8f2598d18b6b878b01fe3e3
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-11-23 16:21:32 +0530

    fixed: fetch user prompts

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,12 +1,12 @@
 import Prompt from "@models/prompt";
 import { connectDB } from "@utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
     try {
         await connectDB();
-
-        const prompts = await Prompt.find({}).populate("creator");
-
+        const prompts = await Prompt.find({ creator: params.id }).populate(
+            "creator"
+        );
         return new Response(JSON.stringify(prompts), { status: 200 });
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 });
